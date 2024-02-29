c85d3988fb9469f18e6036ebdb0809aca56e7f04
hpaloma <122404442+hpaloma@users.noreply.github.com>
2024-02-29 14:05:02 -0700

    stylesheet

---
+++ personalized/personalized.js
@@ -0,0 +1,24 @@
+// script.js
+const videoThumbnail = document.getElementById('videoThumbnail');
+const videoModal = document.getElementById('videoModal');
+const closeModalButton = videoModal.querySelector('.close');
+
+videoThumbnail.addEventListener('click', () => {
+    videoModal.style.display = 'block';
+});
+
+closeModalButton.addEventListener('click', () => {
+    videoModal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target === videoModal) {
+        videoModal.style.display = 'none';
+    }
+});
+
+videoThumbnail.addEventListener('click', () => {
+  videoModal.style.display = 'block';
+  const videoElement = videoModal.querySelector('video');
+  videoElement.play(); // Autoplay the video
+});
